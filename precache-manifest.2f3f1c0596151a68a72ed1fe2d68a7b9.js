a63a01715e8d1f4483515c0fd964f78f696f4804
jtiala <joonas.tiala@gmail.com>
2019-10-01 06:52:13 +0000

    Deploying to gh-pages from master 5e9d94b2d909c22482ee9d42856194c9973a7ff4

---
+++ precache-manifest.2f3f1c0596151a68a72ed1fe2d68a7b9.js
@@ -0,0 +1,57 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38227ffb6957e7cb6465f536a7864201",
+    "url": "/./index.html"
+  },
+  {
+    "revision": "3906c37a3d4b0f337bf64ac3e37b8b4c",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "9ad977f1c85371140ddccfbe0e51976f",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "8b2706a3ce61e8f55887079509bad25e",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "e36975bf16da097896b26eac89a6711d",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "9dd869f69f7179ded08d80d6ce7562af",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "7ee30700f4939a97f0dde9c59a0d5a95",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "d223e7b151aba805d7de84c0f9854140",
+    "url": "/favicon.ico"
+  },
+  {
+    "url": "/main.a01cc233cad32ef8cd1e.css"
+  },
+  {
+    "url": "/main.a01cc233cad32ef8cd1e.js"
+  },
+  {
+    "revision": "2fbf6014a7ed83e35c0d79d10188fcc4",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "2746f0d3599151b33e9044d5085a2200",
+    "url": "/site.webmanifest"
+  },
+  {
+    "url": "/vendor.a01cc233cad32ef8cd1e.css"
+  },
+  {
+    "url": "/vendor.a01cc233cad32ef8cd1e.js"
+  },
+  {
+    "url": "/vendor.a01cc233cad32ef8cd1e.js.LICENSE"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.2f3f1c0596151a68a72ed1fe2d68a7b9.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "reducks-starter"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
